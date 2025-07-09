9abc886a552867b27e45a9d09b605b17e8896da4
kraglr <klaguilar.1doc@gmail.com>
2025-07-09 16:19:51 +0800

    Update SelectCaseTypeModal.jsx

--- src/features/modals/SelectCaseTypeModal.jsx
+++ src/features/modals/SelectCaseTypeModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ClearIcon from "@mui/icons-material/Clear";
 const SelectCaseTypeModal = ({ hideModal, data }) => {
   return (
     <div
@@ -13,60 +13,27 @@ const SelectCaseTypeModal = ({ hideModal, data }) => {
         aria-hidden="true"
       ></div>
 
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div class="sm:flex sm:items-start">
-                <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
-                  <svg
-                    class="size-6 text-red-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                    data-slot="icon"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-                    />
-                  </svg>
-                </div>
-                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <h3
-                    class="text-base font-semibold text-gray-900"
-                    id="dialog-title"
-                  >
-                    Deactivate account
-                  </h3>
-                  <div class="mt-2">
-                    <p class="text-sm text-gray-500">
-                      Are you sure you want to deactivate your account? All of
-                      your data will be permanently removed. This action cannot
-                      be undone.
-                    </p>
-                  </div>
-                </div>
-              </div>
+      <div class="flex fixed inset-0 z-10 min-w-screen min-h-screen overflow-y-auto align-middle items-center justify-center">
+        <div className="modal flex flex-col  w-1/3 my-auto ">
+          <div className="modal-header  flex justify-end items-end align-middle w-full py-3 bg-gray-900 text-white-300 rounded-t-lg">
+            <ClearIcon />
+          </div>
+          <div className="modal-body flex bg-gray-200 justify-center items-center align-middle p-2">
+            <div className="relative  w-20 h-20">
+              {/* Spinner ring */}
+              <div
+                className="absolute inset-0 border-4 border-gray-900 border-t-transparent rounded-full animate-spin"
+                role="status"
+              ></div>
+
+              {/* Exclamation mark fixed in center */}
+              <span className="absolute inset-0 flex items-center justify-center text-gray-900 text-6xl font-bold">
+                !
+              </span>
             </div>
-            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Deactivate
-              </button>
-              <button
-                type="button"
-                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                onClick={hideModal}
-              >
-                Cancel
-              </button>
-            </div>
+          </div>
+          <div className="modal-footer border-t-1 text-gray-900 border-gray-300 bg-gray-100 rounded-b-lg">
+            asdasd
           </div>
         </div>
       </div>
