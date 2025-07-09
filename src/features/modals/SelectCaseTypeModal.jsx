97318cb26b01e40f1633d4190a8da3fdb391f946
kraglr <klaguilar.1doc@gmail.com>
2025-07-09 17:20:59 +0800

    Update SelectCaseTypeModal.jsx

--- src/features/modals/SelectCaseTypeModal.jsx
+++ src/features/modals/SelectCaseTypeModal.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ClearIcon from "@mui/icons-material/Clear";
+import { useNavigate } from "react-router";
+
 const SelectCaseTypeModal = ({ hideModal, data }) => {
+  const navigate = useNavigate();
   return (
     <div
-      class="relative z-[9999]"
+      class="relative z-[9999] min-h-screen"
       aria-labelledby="dialog-title"
       role="dialog"
       aria-modal="true"
@@ -14,11 +17,11 @@ const SelectCaseTypeModal = ({ hideModal, data }) => {
       ></div>
 
       <div class="flex fixed inset-0 z-10 min-w-screen min-h-screen overflow-y-auto align-middle items-center justify-center">
-        <div className="modal flex flex-col  w-1/3 my-auto ">
-          <div className="modal-header  flex justify-end items-end align-middle w-full py-3 bg-gray-900 text-white-300 rounded-t-lg">
-            <ClearIcon />
+        <div className="modal flex flex-col 2xl:w-1/5 xl:w-1/4 lg:w-1/3  w-1/2 my-auto ">
+          <div className="modal-header  flex justify-end items-end px-2 align-middle w-full py-3 bg-gray-900 text-white-300 rounded-t-lg">
+            <ClearIcon onClick={hideModal} />
           </div>
-          <div className="modal-body flex bg-gray-200 justify-center items-center align-middle p-2">
+          <div className="modal-body flex flex-col space-y-5 rounded-b-lg bg-gray-200 text-gray-900 justify-center items-center align-middle px-2 py-4 transition-all ease-in-out duration-300">
             <div className="relative  w-20 h-20">
               {/* Spinner ring */}
               <div
@@ -31,10 +34,37 @@ const SelectCaseTypeModal = ({ hideModal, data }) => {
                 !
               </span>
             </div>
+
+            <div className="grid lg:grid-cols-3 grid-cols-1 justify-center items-center lg:gap-y-0 gap-y-3">
+              <button
+                className="col-span-1 px-2 py-1 border-1 lg:rounded-l-md lg:rounded-r-none rounded-md  "
+                onClick={() => {
+                  navigate("/patient-information");
+                }}
+              >
+                Emergency
+              </button>
+              <button
+                className="col-span-1 px-2 py-1 border-1 lg:rounded-none rounded-md"
+                onClick={() => {
+                  navigate("/patient-information");
+                }}
+              >
+                Inpatient
+              </button>
+              <button
+                className="col-span-1 px-2 py-1 border-1 lg:rounded-r-md lg:rounded-l-none rounded-md  "
+                onClick={() => {
+                  navigate("/patient-information");
+                }}
+              >
+                Outpatient
+              </button>
+            </div>
           </div>
-          <div className="modal-footer border-t-1 text-gray-900 border-gray-300 bg-gray-100 rounded-b-lg">
-            asdasd
-          </div>
+          {/* <div className="modal-footer border-t-1 text-gray-900 border-gray-300 bg-gray-100 rounded-b-lg">
+            
+          </div> */}
         </div>
       </div>
     </div>
